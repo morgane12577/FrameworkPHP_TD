9a3d6100fa8c71a47710c283771dfd971a6cc5d8
morgane messager <22000880@etu.unicaen.fr>
2022-03-24 15:30:28 +0100

    panier

--- app/controllers/StoreController.php
+++ app/controllers/StoreController.php
@@ -11,16 +11,20 @@ namespace controllers;
  use Ubiquity\attributes\items\router\Get;
  use Ubiquity\orm\DAO;
  use Ubiquity\orm\repositories\ViewRepository;
+ use Ubiquity\utils\http\USession;
 
-class StoreController extends \controllers\ControllerBase{
-    private ViewRepository $repo;
+ #[Route(path: '/')]
+ class StoreController extends \controllers\ControllerBase{
+   private $count = 0;
+
+
 
     public function initialize() {
         parent::initialize();
 
     }
 
-    #[Route('_default', name: 'home')]
+    #[Route(path:'_default', name: 'home')]
     public function index(){
         $sections = DAO::getAll(Section::class);
         $this->loadView('StoreController/index.html',['sections'=>$sections] );
@@ -30,7 +34,8 @@ class StoreController extends \controllers\ControllerBase{
     #[Get (path : "section/{id}",name: 'store.section')]
     public function getSection(int $id){
         $section = DAO::getById(Section::class,$id);
-        $this->loadView('StoreController/getSection.html',['section'=>$section]);
+        $num = DAO::count(Product::class);
+        $this->loadView('StoreController/getSection.html',['section'=>$section, 'cart' => USession::get('cart',["nb"=>0,"montant"=>0]),'count'=>$num]);
     }
 
     #[Route (path: 'allProducts', name: 'store.allProducts')]
@@ -38,6 +43,19 @@ public function getAllProduct()
     {
         $products = DAO::getAll(Product::class);
         $this->loadView('StoreController/getAllProduct.html');
+
+
+    }
+
+    #[Get (path: 'addToCart/{id}/{count}', name: 'store.addToCart')]
+    public function addToCart (int $id, int $count){
+        $montant = DAO::getById(Product::class,$id);
+        $cart = USession::get('cart',["nb"=>0,"montant"=>0]);
+        $cart[]=[$id, $count];
+        $cart["nb"] ++;
+        $cart["montant"] = $cart["montant"] + $montant->getPrice();
+        USession::set('cart',$cart);
+        $this->count ++;
 
 
     }
